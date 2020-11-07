c18e27dea6e89eb12b6bd50478830d9515be3e8c
Simon Panek <paneks19@gmail.com>
2020-11-06 21:05:50 -0800

    Server is displaying correctly to local host 3000

--- server.js
+++ server.js
@@ -6,17 +6,19 @@ const express = require('express'); //3rd party dependency
 const app = express (); //application constant
 const PORT = process.env.PORT || 3000; //application constant
 
-//app.use(express.static('./public/index')); //express will serve the static directory "public"
+app.use(express.static('./public')); //express will serve the static directory "public"
 
-app.get('/public', (request, response) => {
-  response.sendFile(__dirname + '/public/index.html'); //servers our webpage
-})
+// app.get('/', (request, response) => {
+//   response.sendFile('/public/index.html'); //servers our webpage
+// })
 
 // app.get('/about-us', (request, response) => { //this works
 //   response.send('i am the about us webpage');
 // });
 
-app.listen(PORT); // sets up access for incoming traffic on a port of heroku's choosing with local 3000 as a backup
+app.listen(PORT, () => {
+  console.log('Server up on port', `${PORT}`);
+}); // sets up access for incoming traffic on a port of heroku's choosing with local 3000 as a backup
 
 
 // app.get('/hello', (request, response) => { //this works too
